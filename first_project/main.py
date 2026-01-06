f7ae90c5c104f73103f1040b492e890b7db6f57b
maabolilab <maabolilab@gmail.com>
2026-01-06 22:35:48 +0000

    school admin project

--- first_project/main.py
+++ first_project/main.py
@@ -1,12 +1,68 @@
 from student import Student
 from teacher import Teacher
+from school_admin import School_Admin
 
-harmeet = Student("Harmeet", 34, "Moga", "9999999999", ["maths", "science", "sst", "punjabi"], 32, 10)
-harkirat = Student("Harkirat", 35, "Moga", "44455566", ["maths", "science", "sst", "punjabi"], 33, 10)
+def display_menu():
+    print("-"*60)
+    print("Press 1 to add Student")
+    print("Press 2 to add Teacher")
+    print("Press 3 to delete Student")
+    print("Press 4 to delete Teacher")
+    print("Press 5 to get all list of Students")
+    print("Press 6 to get all list of teachers")
+    print("Press any other key to quit.")
+    print("-"*60)
 
-print(harmeet)
-# print(harkirat)
+def get_user_details():
+    name = input("Enter Name: ")
+    age = input("Enter Age: ")
+    address = input("Enter address: ")
+    phone_number = input("Enter Phone Number: ")
+    subjects = input("Enter Subjects: ")
 
-manjeet = Teacher("Manjeet", 45, "Moga", 545454545, ["maths"], 445566, [9, 10])
+    return {
+        "name": name,
+        "age": age,
+        "address": address,
+        "phone_number": phone_number,
+        "subjects": subjects
+    }
 
-print(manjeet)
+admin = School_Admin()
+
+while True:
+    display_menu()
+    value = input("Enter your value: ")
+    match value:
+        case "1":
+            print("Enter Students Details: ")
+            user_details = get_user_details()
+            roll_no = input("Enter Roll No: ")
+            class_ = input("Enter Your Class: ")
+            student = Student(
+                user_details['name'], user_details['age'], user_details['address'],
+                user_details['phone_number'], user_details['subjects'], roll_no, class_
+            )
+            admin.add_student(student)
+            5
+        case "2":
+            print("Enter Teacher Details: ")
+            user_details = get_user_details()
+            employee_id = input("Enter Employee Id: ")
+            classes = input("Enter Your Classes: ")
+            teacher = Teacher(
+                user_details['name'], user_details['age'], user_details['address'],
+                user_details['phone_number'], user_details['subjects'], employee_id, classes
+            )
+            admin.add_teacher(teacher)
+
+        case "5":
+            for stundent in admin.get_all_students():
+                print(student)
+
+        case "6":
+            for teacher in admin.get_all_teachers():
+                print(teacher)
+        case _:
+            print("Exit")
+            break
---
+++ first_project/school_admin.py
@@ -0,0 +1,25 @@
+from student import Student
+from teacher import Teacher
+
+class School_Admin:
+    def __init__(self):
+        self.students = {}
+        self.teachers = {}
+
+    def add_student(self, student: Student):
+        self.students[student.roll_no] = student
+
+    def add_teacher(self, teacher: Teacher):
+        self.students[teacher.employee_id] = teacher
+
+    def delete_teacher(self, employee_id):
+        return self.teachers.pop(employee_id, None)
+    
+    def delete_student(self, roll_no):
+        return self.students.pop(roll_no, None)
+    
+    def get_all_students(self):
+        return list(self.students.values())
+    
+    def get_all_teachers(self):
+        return list(self.teachers.values())
